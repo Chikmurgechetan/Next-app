7ad3c5e5c4beb89ad39724cd9ee059f6b5543e0c
Chikmurgechetan <chikmurgechetan@gmail.com>
2023-09-03 00:31:08 +0530

    new page using Link

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,7 +1,18 @@
 // our-domain.com/aboutus
 
-function AboutUS(){
-    return <h1>The AboutUs Page</h1>
-  }
-  
-  export default AboutUS;
+import Link from "next/link";
+
+function AboutUS() {
+  return (
+    <div>
+      <h1>The AboutUs Page</h1>
+      <ul>
+        <li><Link href="aboutus/1">Yash</Link></li>
+        <li><Link href="aboutus/2">Vaibhav</Link></li>
+        <li><Link href="aboutus/3">Suresh</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export default AboutUS;
